b71e1f8f01206c695464d0ec29b062d4b9d15faf
Jaxson Wang <i@iiong.com>
2022-02-27 15:43:50 +0800

    add comments count

--- 2021-SoldOut/src/js/bootstrap/comments.js
+++ 2021-SoldOut/src/js/bootstrap/comments.js
@@ -7,9 +7,23 @@ export default () => {
       path: 'https://cdn.jsdelivr.net/npm/twikoo@1.4.18/dist/twikoo.all.min.js'
     }
   ]).then(() => {
+    const envId = 'twikoo-8gis88nx761b6095'
     window.twikoo.init({
       el: '#vcomments',
-      envId: 'twikoo-8gis88nx761b6095'
+      envId: envId
     })
+    // 加载评论数量
+    const comments = document.querySelector('#twikoo_comments')
+    if (comments !== null) {
+      window.twikoo.getCommentsCount({
+        envId: envId,
+        urls: [window.location.pathname],
+        includeReply: false
+      }).then(response => {
+        comments.innerText = response[0].count || 0
+      }).catch(error => {
+        console.error(error)
+      })
+    }
   })
 }
